1216841f88c6920a2231e9656087050da0abc982
Josephat Onyekwelu <onyekwelu.jo@gmail.com>
2022-09-30 09:11:07 -0700

    Technical interview preparation

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -15,8 +15,8 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
 
-    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    list_keys = list(rom_n.keys())
+    rom_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    list_keys = list(rom_num.keys())
 
     num = 0
     last_rom = 0
@@ -25,11 +25,12 @@ def roman_to_int(roman_string):
     for ch in roman_string:
         for r_num in list_keys:
             if r_num == ch:
-                if rom_n.get(ch) <= last_rom:
+                if rom_num.get(ch) <= last_rom:
                     num += to_subtract(list_num)
-                    list_num = [rom_n.get(ch)]
+                    list_num = [rom_num.get(ch)]
                 else:
-                    list_num.append(rom_n.get(ch))
-                last_rom = rom_n.get(ch)
-        num += to_subtract(list_num)
-        return (num)
+                    list_num.append(rom_num.get(ch))
+                
+                last_rom = rom_num.get(ch)
+    num += to_subtract(list_num)
+    return (num)
