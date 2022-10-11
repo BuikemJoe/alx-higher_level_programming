8c80aab9c173bf03758e552637db36d5b5236e2f
BuikemJoe <onyekwelu.jo@gmail.com>
2022-10-11 09:39:07 -0700

    Write a class Node that defines a node of a singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -58,7 +58,7 @@ class SinglyLinkedList:
             self.__head = new
         elif self.__head.data > value:
             new.next_node = self.__head
-            self.__head = 
+            self.__head = new 
         else:
             tmp = self.__head
             while (tmp.next_node is not None and
@@ -67,11 +67,11 @@ class SinglyLinkedList:
             new.next_node = tmp.next_node
             tmp.next_node = new
 
-        def __str__(self):
+    def __str__(self):
         """Define the print() representation of a SinglyLinkedList."""
-        values = []
-        tmp = self.__head
-        while tmp is not None:
-             values.append(str(tmp.data))
-             tmp = tmp.next_node
+    values = []
+    tmp = self.__head
+    while tmp is not None:
+        values.append(str(tmp.data))
+        tmp = tmp.next_node
         return ('\n'.join(values))
