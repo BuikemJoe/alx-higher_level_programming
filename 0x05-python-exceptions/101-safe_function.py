381a5eaf8beefb233d4c8f9b4703cae0fa5f50b4
BuikemJoe <onyekwelu.jo@gmail.com>
2022-10-11 01:35:31 -0700

    Write a function that executes a function safely

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,9 +4,18 @@ import sys
 
 
 def safe_function(fct, *args):
+    """Prints an integer with "{:d}".format().
+    If a ValueError message is caught, a corresponding
+    message is printed to standard error.
+    Args:
+        value (int): The integer to print.
+    Returns:
+        If a TypeError or ValueError occurs - None
+        Otherwise - res
+    """
     try:
         res = fct(*args)
-    except Exception as ex:
+    except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return None
     else:
